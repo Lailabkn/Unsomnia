23546aee5452bf18f00e69cbfe4cc793db1fda13
Fatih20 <fnri39@protonmail.com>
2022-08-15 22:30:32 +0700

    Fix font.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,7 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import Header from "../components/elements/header";
-import styles from "../styles/Home.module.css";
 import styled from "styled-components";
 import BaseLayout from "../components/layout/base";
-import { QueryKey, useQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { getMe } from "../utils/api";
 import { UserOpaque } from "../types/types";
 import LoadingRotation from "../components/elements/loading";
@@ -23,16 +19,13 @@ const Container = styled.div`
 `;
 
 const MainTitle = styled.h2`
-  font-family: "Averia Serif Libre", "Sans-Serif";
   font-size: 3rem;
 `;
 
-const SubTitle = styled.h3`
-  font-family: "Averia Serif Libre", "Sans-Serif";
-`;
+const SubTitle = styled.h3``;
 
 const Button = styled.button`
-  background-color: yellow;
+  background-color: #ffe16b;
   border-radius: 1rem;
   padding: 0.5em;
   text-align: center;
@@ -42,28 +35,37 @@ const Button = styled.button`
 const Home: NextPage = () => {
   const router = useRouter();
   const { data, status } = useQuery<UserOpaque | undefined>(["me"], getMe, {
+    refetchOnMount: false,
+    refetchOnReconnect: false,
+    refetchOnWindowFocus: false,
     retry: 1,
   });
+
+  // return (
+  //   <BaseLayout>
+  //     <LoadingRotation />
+  //   </BaseLayout>
+  // );
+
+  if (status === "loading") {
+    return (
+      <BaseLayout>
+        <LoadingRotation />
+      </BaseLayout>
+    );
+  }
 
   if (status === "success") {
     window.location.href = "/alarms";
   }
 
-  if (status === "error") {
-    return (
-      <BaseLayout>
-        <Container>
-          <MainTitle>Insomnia?</MainTitle>
-          <SubTitle>We can help with that</SubTitle>
-          <Button onClick={() => router.push("/login")}>Join Now</Button>
-        </Container>
-      </BaseLayout>
-    );
-  }
-
   return (
     <BaseLayout>
-      <LoadingRotation />
+      <Container>
+        <MainTitle>Insomnia?</MainTitle>
+        <SubTitle>We can help with that</SubTitle>
+        <Button onClick={() => router.push("/login")}>Join Now</Button>
+      </Container>
     </BaseLayout>
   );
 };
